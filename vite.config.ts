932fb065439a1221c2355c0cd13e72edbc3a759f
Aditya Jayant Wani <120404041+aadi1003@users.noreply.github.com>
2025-12-21 22:48:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: "::",
-    port: 8080,
+    host: "::"
   },
   plugins: [react()],
-   base: process.env.NODE_ENV === 'production' 
-    ? '/Report-Generation-System/'  // Production: GitHub Pages
-    : '/',  
+   
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
